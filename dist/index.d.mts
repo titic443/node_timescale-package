e1c1a91425baee1fc4a775c308166b53ce80cbef
titi.ea <titi.cha@energyabsolute.co.th>
2024-05-13 15:06:28 +0700

    update query to commit

--- dist/index.d.mts
+++ dist/index.d.mts
@@ -13,6 +13,8 @@ declare class TimeScaleSequalize extends Sequelize {
     constructor(options?: CustomSequalizeOpt);
     addConstraint(table: string, constraintName: string, constraint1?: string, constraint2?: string): Promise<void>;
     createHyperTable(table: string, constraint: string, interval?: number): Promise<void>;
+    createTrigger(name: string, table: string, funcName: string): Promise<void>;
+    createFunc(name: string, table: string, column: string[], criteria: string[], constraint?: string): Promise<void>;
 }
 
 export { TimeScaleSequalize };
--- dist/index.mjs
+++ dist/index.mjs
@@ -11,6 +11,7 @@ var TimeScaleSequalize = class extends Sequelize {
           `
             ALTER TABLE public.${table}
             ADD CONSTRAINT ${constraintName} UNIQUE ("${constraint1}", "${constraint2}");
+            COMMIT;
             `,
           {
             raw: true
@@ -21,6 +22,7 @@ var TimeScaleSequalize = class extends Sequelize {
           `
               ALTER TABLE public.${table}
               ADD CONSTRAINT ${constraintName} UNIQUE ("${constraint1}");
+              COMMIT;
               `,
           {
             raw: true
@@ -35,7 +37,7 @@ var TimeScaleSequalize = class extends Sequelize {
     try {
       if (interval) {
         await this.query(
-          `SELECT create_hypertable('${table}', '${constraint}', chunk_time_interval => INTERVAL '${interval.toString()} hour');`,
+          `SELECT create_hypertable('${table}', '${constraint}', chunk_time_interval => INTERVAL '${interval.toString()} day');`,
           {
             raw: true
           }
@@ -47,6 +49,103 @@ var TimeScaleSequalize = class extends Sequelize {
             raw: true
           }
         );
+      }
+    } catch (err) {
+      throw err;
+    }
+  }
+  async createTrigger(name, table, funcName) {
+    try {
+      const q = `
+      CREATE TRIGGER ${name}
+      AFTER INSERT ON "public"."${table}"
+      FOR EACH ROW
+      EXECUTE FUNCTION ${funcName}();
+      `;
+      await this.query(q);
+    } catch (err) {
+      throw err;
+    }
+  }
+  async createFunc(name, table, column, criteria, constraint) {
+    try {
+      let setColumn = "";
+      for (let i = 0; i < column.length; i++) {
+        if (column.length - 1 == i) {
+          setColumn += `"${column[i]}" = NEW. "${column[i]}"`;
+        } else {
+          setColumn += `"${column[i]}" = NEW. "${column[i]}",
+	`;
+        }
+      }
+      let setCriteria = "";
+      for (let i = 0; i < criteria.length; i++) {
+        if (criteria.length - 1 == i) {
+          setCriteria += `"${criteria[i]}" = NEW. "${criteria[i]}";`;
+        } else {
+          setCriteria += `"${criteria[i]}" = NEW. "${criteria[i]}" and 
+	`;
+        }
+      }
+      let newC = column.map((value) => `"${value}"`);
+      let newCr = column.map((value) => `NEW. "${value}"
+	`);
+      let exCr = "";
+      for (let i = 0; i < column.length; i++) {
+        if (column.length - 1 == i) {
+          exCr += `"${column[i]}" = EXCLUDED."${column[i]}";`;
+        } else {
+          exCr += `"${column[i]}" = EXCLUDED."${column[i]}",
+	`;
+        }
+      }
+      if (constraint) {
+        let q = `
+        CREATE OR REPLACE FUNCTION ${name} ()
+        RETURNS TRIGGER
+        AS $$
+      BEGIN
+        UPDATE
+          "public"."${table}"
+        SET
+        ${setColumn}
+        WHERE
+        ${setCriteria}
+        IF NOT FOUND THEN
+          INSERT INTO "public"."${table}" (${newC})
+            VALUES(${newCr})
+        ON CONFLICT ON CONSTRAINT "${constraint}" DO UPDATE
+        SET
+          ${exCr}
+        END IF;
+        RETURN NULL;
+      END;
+      $$
+      LANGUAGE plpgsql;
+      `;
+        this.query(q);
+      } else {
+        let q = `
+        CREATE OR REPLACE FUNCTION ${name} ()
+        RETURNS TRIGGER
+        AS $$
+      BEGIN
+        UPDATE
+          "public"."${table}"
+        SET
+        ${setColumn}
+        WHERE
+        ${setCriteria}
+        IF NOT FOUND THEN
+          INSERT INTO "public"."${table}" (${newC})
+            VALUES(${newCr});
+        END IF;
+        RETURN NULL;
+      END;
+      $$
+      LANGUAGE plpgsql;
+      `;
+        this.query(q);
       }
     } catch (err) {
       throw err;
